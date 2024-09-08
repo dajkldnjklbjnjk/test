7024f73f1853310a7de781e5f0d6b7ae35e48e32
dajkldnjklbjnjk <2998389122@qq.com>
2024-09-08 22:24:40 +0800

    My exercise

---
+++ 常用变量.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+	{
+	int a=0;
+    int b=20;
+    int age=a+b;
+    printf("%d\n",age);
+    return 0;
+}
+
+\\数据类型
+    int main()
+	{
+	printf("%d\n",seizof(char));
+	printf("%d\n",seizof(int)) ;
+	printf("%d\n",seizof(short));
+	printf("%d\n",seizof(long));
+	printf("%d\n",seziof(long long));
+	printf("%d\b",seziof(float));
+	printf("%D\n",seziof(double));
+	return 0;
+}
+
+ 
